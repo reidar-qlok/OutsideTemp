50899da91720bd05ece2d9ea6c5c1c078cdbb15d
Reidar Nilsen <4reidar@gmail.com>
2023-09-08 08:40:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OutsideTemp
+{
+    internal class Program
+    {
+        // Ett övnings exempel utan felhantering, vi kommer till det senare
+        // Det man läser in från en console app blir alltid text (string)
+        // Jag använder int.Parse(string) för att porta svaret över till ett heltal
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, vad är det för temeratur där du bor i dag");
+            string answer = Console.ReadLine();
+            int temp = int.Parse(answer);
+            if(temp >=30)
+            {
+                Console.WriteLine("Varmt");
+            }
+            else if (temp < 30 && temp > 20)
+            {
+                Console.WriteLine("Sommar");
+            }
+            else
+            {
+                Console.WriteLine("Höst/Vinter");
+            }
+        }
+    }
+}
